72cd5c60debb20f808cc1e4204cb2594c1220397
Cull Obsidian <77494299+cullobsidian7@users.noreply.github.com>
2021-12-30 17:19:00 +0530

    Solution for stickers giving same results two times (#40)
    * Update stickers.py
    
    * drunk while coding
    
    * Update stickers.py
    
    Always  test before you push

--- AstrakoBot/modules/stickers.py
+++ AstrakoBot/modules/stickers.py
@@ -39,23 +39,22 @@ def stickerid(update: Update, context: CallbackContext):
 
 def cb_sticker(update: Update, context: CallbackContext):
     msg = update.effective_message
+    stp = ""
     split = msg.text.split(" ", 1)
     if len(split) == 1:
         msg.reply_text("Provide some name to search for pack.")
         return
     text = requests.get(combot_stickers_url + split[1]).text
     soup = bs(text, "lxml")
-    results = soup.find_all("a", {"class": "sticker-pack__btn"})
-    titles = soup.find_all("div", "sticker-pack__title")
-    if not results:
-        msg.reply_text("No results found :(.")
-        return
-    reply = f"Stickers for *{split[1]}*:"
-    for result, title in zip(results, titles):
-        link = result["href"]
-        reply += f"\n• [{title.get_text()}]({link})"
-    msg.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
-
+    results = soup.find_all("div", {"class": "sticker-pack__header"})
+    for pack in results:
+        if pack.button:
+            title_ = (pack.find("div", {"class": "sticker-pack__title"})).text
+            link_ = (pack.a).get("href")
+            stp += f"\n• [{title_}]({link_})"
+    if not stp:
+            stp = "Stickers not found"
+    msg.reply_text(stp, parse_mode=ParseMode.MARKDOWN)
 
 def getsticker(update: Update, context: CallbackContext):
     bot = context.bot
