3c995179e9fbb4e6baaa1cbe19d58f6098fa7229
Luke <67458252+itsLuuke@users.noreply.github.com>
2022-01-23 20:50:26 +0200

    super_users: bug fixes (#44)
    fix bug while adding dev as whitelist or support

--- AstrakoBot/modules/super_users.py
+++ AstrakoBot/modules/super_users.py
@@ -132,6 +132,10 @@ def addsupport(
     with open(ELEVATED_USERS_FILE, "r") as infile:
         data = json.load(infile)
 
+    if user_id in DEV_USERS:
+        message.reply_text("Huh? he is more than support!")
+        return ""
+
     if user_id in SUDO_USERS:
         if user.id in DEV_USERS:
             rt += "Demoted from sudo to support user"
@@ -190,6 +194,10 @@ def addwhitelist(update: Update, context: CallbackContext) -> str:
 
     with open(ELEVATED_USERS_FILE, "r") as infile:
         data = json.load(infile)
+
+    if user_id in DEV_USERS:
+        message.reply_text("Huh? he is more than whitelist!")
+        return ""
 
     if user_id in SUDO_USERS:
         if user.id in DEV_USERS:
