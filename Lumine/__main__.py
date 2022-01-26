73818036846fa84354d196ad7cd6001d486045f5
titu3e <75441242+titu3e@users.noreply.github.com>
2022-01-26 09:54:11 +0530

    Update __main__.py

--- Lumine/__main__.py
+++ Lumine/__main__.py
@@ -101,12 +101,11 @@ And the following:
 
 HELP_IMG = "https://te.legra.ph/file/9b3f9cff62cda296279cc.mp4"
 
- LUM_ING= (
+LUM_ING= (
           "https://te.legra.ph/file/e690403cba45611ca7daf.mp4",
           "https://te.legra.ph/file/50bf031019f63482031df.mp4",
-          "https://c.tenor.com/M25TmVGyi0QAAAAC/zhongling.gif"    
+          "https://c.tenor.com/M25TmVGyi0QAAAAC/zhongling.gif",    
           )
-
 
 Lumine_IMG = "https://telegra.ph/file/d9fd3e727627b10179308.jpg"
 
